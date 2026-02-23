12f703952b5927bd9ea9cfd36576110dbd739fe2
kasidish <kasidish.b@gmail.com>
2026-02-23 22:08:44 +0700

    Cubesat

--- CubesatProject.ino
+++ CubesatProject.ino
@@ -17,7 +17,7 @@ QueueHandle_t dataQueue;
 void setup() {
     Serial.begin(115200);
     delay(1000);
-    Serial.println("--- ESP32-S3 OOP Cubesat Refactor ---");
+    Serial.println("ESP32-S3 OOP Cubesat");
 
     // Initialize Shared Queue
     dataQueue = xQueueCreate(10, sizeof(MeasurementData));
